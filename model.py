e1fa557b0ea3e31249ee0e01301d66026027b10e
tokenie <108528142+tokenie@users.noreply.github.com>
2025-12-18 10:40:15 -0600

    model and training templates

---
+++ model.py
@@ -0,0 +1,28 @@
+from tensorflow.keras import layers, Model, Input
+
+def build_model(num_classes, embedding_dim=64):
+    inputs = Input(shape=(32, 32, 3))
+
+    x = layers.Conv2D(32, 3, activation="relu", padding="same")(inputs)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(64, 3, activation="relu", padding="same")(x)
+    x = layers.MaxPooling2D()(x)
+
+    x = layers.Conv2D(128, 3, activation="relu", padding="same")(x)
+    x = layers.GlobalAveragePooling2D()(x)
+
+
+    # embedding = layers.Dense(
+    #     embedding_dim,
+    #     activation=None,
+    #     name="embedding"
+    # )(x)
+
+    # x = layers.BatchNormalization()(embedding)
+    # x = layers.ReLU()(x)
+
+    outputs = layers.Dense(num_classes, activation="softmax")(x)
+
+    model = Model(inputs, outputs)
+    return model
---
+++ train.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from model import build_model
+from data import load_dataset
+
+X_train, y_train, class_names = load_dataset("./Dungeon_Crawler_Data")
+
+model = build_model(num_classes=len(class_names))
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+history = model.fit(
+    X_train, y_train,
+    batch_size=32,
+    epochs=20,
+    validation_split=0.1
+)
